f62be3a9f4b69223c4cfdd574c6a422a641de7ba
Kaylla Roland <kayllar.@macbookpro.mynetworksettings.com>
2025-09-22 13:54:02 -0400

    Fix layout params typing in [cid]/layout.tsx

--- src/app/(Kambaz)/Courses/[cid]/layout.tsx
+++ src/app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,19 +1,29 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
- const { id } = await params;
- return (
-   <div id="wd-courses">
-     <h2>Courses {id}</h2>
-     <hr />
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="200"> <CourseNavigation /> </td>
-           <td valign="top" width="100%"> {children} </td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
-);}
+
+export default function CoursesLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { cid: string };
+}) {
+  return (
+    <div id="wd-courses">
+      <h2>Course {params.cid}</h2>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="200">
+              <CourseNavigation />
+            </td>
+            <td valign="top" width="100%">
+              {children}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
