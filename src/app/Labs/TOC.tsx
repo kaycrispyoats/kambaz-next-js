1c038af7549358eb2ab5c4bff37a9a5d918f74bb
Kaylla Roland <kayllar.@Kayllas-MacBook-Pro.local>
2025-10-06 23:11:19 -0400

    changed link for my github in lab landing page

--- src/app/Labs/TOC.tsx
+++ src/app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+       <NavLink href="https://github.com/kaycrispyoats/kambaz-next-js.git">My GitHub</NavLink>
      </NavItem>
    </Nav>
 );}
