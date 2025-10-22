c1215d45cb41fbd19efdb2eb9ab6c9b5708d2751
Kaylla Roland <kayllar.@Kayllas-MacBook-Pro.local>
2025-10-21 23:02:53 -0400

    added name to lab landing page

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 export default function Labs() {
  return (
    <div id="wd-labs">
-     <h1>Labs</h1>
+     <h1>Labs
+     </h1>
+     Kaylla Roland Section 4
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
